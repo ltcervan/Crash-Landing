9028e54265fbbe7f53f8527d3d52071102012ace
ltcervan <ltcervan@gmail.com>
2023-10-07 14:53:54 -0700

    feat: simplified canvas, added mouse control, added a player, added objects that need to collected for points, added a score board

--- script.js
+++ script.js
@@ -1,133 +1,124 @@
-/** @type {HTMLCanvasElement} */ // Accessing canvas elements in HTML
+// canvas set up
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 3; // to control scroll speed: It is a global game speed
+canvas.width = 800;
+canvas.height = 500;
 
-// ---- Level Diffculties ---- Even listeners ~~~~~~ Optional 1:15min
+let score = 0;
+let gameFrame = 0;
+ctx.font = '50px Georgia';
+// Mouse interactivity
+let canvasPosition = canvas.getBoundingClientRect();
+const mouse = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    click: false
+}
+canvas.addEventListener('mousedown', function (event) {
+    mouse.click = true;
+    mouse.x = event.x - canvasPosition.left;
+    mouse.y = event.y - canvasPosition.top;
+})
+canvas.addEventListener('mouseup', function () {
+    mouse.click = false;
+})
 
-
-//--- appending all layers of parallax background ----
-const backgroundLayer1 = new Image();
-backgroundLayer1.src = 'layer-1.png';
-const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'layer-2.png';
-const backgroundLayer3 = new Image();
-backgroundLayer3.src = 'layer-3.png';
-const backgroundLayer4 = new Image();
-backgroundLayer4.src = 'layer-4.png';
-const backgroundLayer5 = new Image();
-backgroundLayer5.src = 'layer-5.png';
-const backgroundLayer6 = new Image();
-backgroundLayer6.src = 'layer-6.png';
-const backgroundLayer7 = new Image();
-backgroundLayer7.src = 'layer-7.png';
-const backgroundLayer8 = new Image();
-backgroundLayer8.src = 'layer-8.png';
-
-
-/**
- * ================== Making Background Animation ==================
- */
-
-/**
- * The "Layer" class will be used as a blue print to draw each layer
- * and prep it for endless scrolling. Creating 8 image layer objects.
- * each layer will have the same properties except 'image' and 'speed'
- * because we want each layer to move at a different speed from eachother
- */
-class Layer {
-    constructor(image, speedModifier){ // image and speedModifier are the 2 objects that will differ bwteen each image layer object
-        this.x = 0; // Horizontal x coordinate that is set to 0
-        this.y = 0; // Virtical y coordinate that is set to 0
-        this.width = 2400; // Same width for all layers
-        this.height = 700; // Same height for all layers
-        this.x2 = this.width;
-        this.image = image; // This will be the argument we pass into the class 
-        this.speedModifier = speedModifier; // This will be the argument we pass into the class  
-        this.speed = gameSpeed * this.speedModifier;
+//player
+class Player {
+    constructor() {
+        this.x = canvas.width;
+        this.y = canvas.height / 2;
+        this.radius = 50;
+        this.angle = 0;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.frame = 0;
+        this.spriteWidth = 498;
+        this.spriteHeight = 327;
     }
-    update(){ // this will help keep the game speed dynamic rather than static
-        this.speed = gameSpeed * this.speedModifier;
-        if (this.x <= -this.width){
-            this.x = this.width + this.x2 - this.speed;
+    update() {
+        const dx = this.x - mouse.x;
+        const dy = this.y - mouse.y;
+        if (mouse.x != this.x) {
+            this.x -= dx/20;
         }
-        if (this.x2 <= -this.width) {
-            this.x2 = this.width + this.x - this.speed;
+        if (mouse.y != this.y) {
+            this.y -= dy/20;
         }
-        this.x = Math.floor(this.x - this.speed);
-        this.x2 = Math.floor(this.x2 - this.speed);
     }
-    draw(){ // This will draw the image at the coordinates given bellow
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    draw() {
+        if (mouse.click) {
+            ctx.lineWidth = 0.2;
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+        }
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.fillRect(this.x, this.y, this.radius, 10);
     }
 }
-// creating instances of the class Layer using the background-images, to prep for animation
-const layer1 = new Layer(backgroundLayer1, .2); // inputing the 2 arguments (image, speedModifier)
-const layer2 = new Layer(backgroundLayer2, .4);
-const layer3 = new Layer(backgroundLayer3, .5);
-const layer4 = new Layer(backgroundLayer4, .65);
-const layer5 = new Layer(backgroundLayer5, .75);
-const layer6 = new Layer(backgroundLayer6, .8);
-const layer7 = new Layer(backgroundLayer7, .95);
-const layer8 = new Layer(backgroundLayer8, 1);
+const player = new Player
 
-// an array of all the new instances made from class layer
-const allLayers = [layer1, layer2, layer3, layer4, layer5, layer6, layer7, layer8]; 
+// Popping Batterries
+const batteriesArray = [];
+class Battery {
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = canvas.height + 100; //--- Look here to make batteries drop instead of rise
+        this.radius = 25;
+        this.speed = Math.random() * 5 + 1;
+        this.distance;
+    }
+    update(){
+        this.y -= this.speed;
+        const dx = this.x - player.x;
+        const dy = this.y - player.y
 
-function animation() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    allLayers.forEach(layer => {
-        layer.update();
-        layer.draw();
-    })
-    requestAnimationFrame(animation);
-};
-animation();
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+function handleBatteries(){
+    if (gameFrame % 50 == 0){
+        batteriesArray.push(new Battery());
+    }
+    for (let i = 0; i < batteriesArray.length; i++){
+        batteriesArray[i].update();
+        batteriesArray[i].draw();
+    }
+    for (let i = 0; i < batteriesArray.length; i++){
+        if(batteriesArray[i].y < 0 - batteriesArray[i].radius * 2){
+            batteriesArray.splice(i, 1);
+        }
+        if (batteriesArray[i].distance < batteriesArray[i].radius + player.radius){
+            console.log('collision');
+        }
+    }
+}
 
-/**
- * ================== Spawning Random Opponent Objects ===========================
- */
 
-// const numOpponents = 16;
-// const OpponentsArray = [];
+// Animate Loop 
 
-// /** Opponents class is used to 'spawn' random objects at random positions 
-//  * on canvas
-//  */
-// class Opponent {
-//     constructor(){
-//         this.x = Math.random() * canvas.width; // setting x-coordinate that will spawn
-//         this.y = Math.random() * canvas.height;   // setting y-coordinate that will spawn
-//         this.width =  // setting width-size paramenter
-//         this.height = // setting height-size paramenter
-//         this.speed = Math.random() * 4 - 2; // setting a random number with range of 4 that starts at -2
-//     }
-//     update(){ // this function resets the position of the opponent so it generates coordinates (x and y) dependant on speed variable
-//         this.x += this.speed;
-//         this.y += this.speed; 
-//     }
-//     draw(){ // Function used to draw random rectangles that is filled according to the current fillStyle
-//         ctx.fillRect(this.x, this.y, this.width, this.height);
-//     }
-// }
-// /** 
-//  * This for loop will take a given number of desired 
-//  * opponenets and will create an instance of the 
-//  * class Opponents and push that instance to an empty array
-//  *  */ 
-// for (let i = 0; i < numOpponents; i++){
-//     OpponentsArray.push(new Opponent());
-// }
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleBatteries();
+    player.update();
+    player.draw();
+    ctx.fillStyle = 'black';
+    ctx.fillText('score: ' + score, 10, 50);
 
-// function animateOpponent (){
-//     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-//     OpponentsArray.forEach(opponent => {
-//         opponent.update();
-//         opponent.draw();
-//     }) 
-//     requestAnimationFrame(animateOpponent);
-// }
-// animateOpponent();
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate();
